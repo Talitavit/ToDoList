c49c5000bbbd42b27ce829501d743daa1c815702
Talitavit <talitavitoria0107@gmail.com>
2025-02-12 22:40:27 -0300

    feat: add GET, PUT and DELETE methods for tasks

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -1,3 +1,4 @@
+const { SHOWINDEXES } = require("sequelize/lib/query-types");
 const { Task } = require("../models");
 
 class Taskcontrollers {
@@ -7,7 +8,66 @@ class Taskcontrollers {
       const task = await Task.create({ title, description, status, priority });
       return res.status(201).json(task);
     } catch (error) {
-      return res.status(400).json({ error: error.message });
+      return res.status(400).json({ message: "Falha ao cria tarefa!" });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const tasks = await Task.findAll();
+      return res.status(200).json(tasks);
+    } catch (error) {
+      return res.status(400).json({ message: "Falha ao listar tarefas!" });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const task = await Task.findByPk(id);
+
+      if (!task) {
+        return res.status(404).json({ message: "Tarefa nao encontrada" });
+      }
+      return res.status(200).json(task);
+    } catch (error) {
+      return res.status(404).json({ message: "Falha ao encontrar tarefa" });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      const { title, description, status, priority } = req.body;
+
+      await Task.update(
+        { title, description, status, priority },
+        {
+          where: {
+            id: id,
+          },
+        }
+      );
+      return res
+        .status(200)
+        .json({ message: "Tarefa atualizada com sucesso!" });
+    } catch (error) {
+      return res.status(404).json({ message: "Falha ao atualizar tarefa" });
+    }
+  }
+
+  async destroy(req, res) {
+    try {
+      const { id } = req.params;
+
+      await Task.destroy({
+        where: {
+          id: id,
+        },
+      });
+      return res.status(200).json({ message: "Tarefa excluida com sucesso!" });
+    } catch (error) {
+      return res.status(404).json({ message: "Falha ao excluir tarefa" });
     }
   }
 }
