18b0d17f6fafe75020f3701192370779c74db83b
Talita Vitória <156473950+Talitavit@users.noreply.github.com>
2025-03-14 21:01:21 -0300

    chore(deps): resolve package-lock.json merge conflict

--- src/app.js
+++ src/app.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
 const express = require("express");
 const routes = require("./routes");
 
 const app = express();
 
 app.use(express.json());
-app.use("/api", routes);
-=======
-const express = require('express');
-const routes = require("./routes");
-
-const app  = express();
-
-app.use(express.json());
-app.use(routes);    
->>>>>>> 5432b72b35184516a5417e6aaf485f1af0eeba43
+app.use(routes);
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 const app = require("./app.js");
 
 app.listen(3000, () => console.log("Server running"));
-=======
-const app = require ("./app.js");
-
-app.listen(3000, () => console.log("Server running"));
->>>>>>> 5432b72b35184516a5417e6aaf485f1af0eeba43
